e3eb629e457ebfd29fa5a03b659796b77e95a303
Kamil Lepkowski <lepkowskikamil@yahoo.com>
2023-03-30 01:07:07 -0500

    Game class, suits to emojis, draw card functions

--- ninja.py
+++ ninja.py
@@ -9,6 +9,9 @@ class Card:
     def show(self):
         print("{} of {}".format(self.value, self.suit))
 
+    def return_value(self):
+        return self.value
+
 
 class Deck:
     def __init__(self):
@@ -16,8 +19,8 @@ class Deck:
         self.generate_deck()
 
     def generate_deck(self):
-        for suit in ["Hearts", "Clubs", "Diamonds", "Spades"]:
-            for val in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]:
+        for suit in ["♥", "♣", "♦", "♠"]:
+            for val in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]:
                 self.cards.append(Card(suit, val))
 
     def show(self):
@@ -38,10 +41,71 @@ class Player:
         self.down = []
         self.up = []
 
-    def draw(self, deck):
-        self.hand.append(deck.draw_card())
+    def draw_hand(self, deck, cards):
+        for c in range(cards):
+            self.hand.append(deck.draw_card())
+        return self
+
+    def draw_cards_up(self, deck, cards):
+        for c in range(cards):
+            self.up.append(deck.draw_card())
+        return self
+
+    def draw_cards_down(self, deck, cards):
+        for c in range(cards):
+            self.down.append(deck.draw_card())
         return self
 
     def show_hand(self):
         for card in self.hand:
             card.show()
+
+
+class Game:
+    def get_name(self):
+        n = "Enter name for Player: "
+        return input(n)
+
+    def compare_lowest(self):
+        p1 = "Player 1, what is your lowest card? "
+        p1 = input(p1)
+        p2 = "Player 2, what is your lowest card? "
+        p2 = input(p2)
+        if p1 > p2:
+            return 0
+        else:
+            return 1
+
+    def start(self):
+        deck = Deck()
+        deck.shuffle()
+
+        # Assign Player Names
+        player_1 = Player(self.get_name())
+        player_2 = Player(self.get_name())
+
+        # Amount of cards facing up / down and draw cards for each player
+        cards_on_table = input(
+            "How many cards would you like to be face down/face up? ")
+        player_1.draw_cards_up(deck, int(cards_on_table))
+        player_1.draw_cards_down(deck, int(cards_on_table))
+        player_2.draw_cards_up(deck, int(cards_on_table))
+        player_2.draw_cards_down(deck, int(cards_on_table))
+
+        # Amount of cards in hand
+        cards_in_hand = input(
+            "How many cards would you like to have in hand? ")
+        player_1.draw_hand(deck, int(cards_in_hand))
+        player_2.draw_hand(deck, int(cards_in_hand))
+
+        player_1.show_hand()
+
+        # print(Game.compare_lowest(self))
+
+    def main_game(self):
+
+        pass
+
+
+game = Game()
+game.start()
